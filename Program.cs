3d1deadc0f30e2f8965ab0a5981f9ca359a8ae88
BÜŞRA OZAN KARAOZAN <95549258+karaozan09@users.noreply.github.com>
2022-10-23 16:14:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaftaucİkinciKısım
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir sayı giriniz:");
+         int sayı1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ikinci sayıyı giriniz:");
+          int sayı2 = Convert.ToInt32(Console.ReadLine());
+
+            if (sayı1 % sayı2 == 0)
+            {
+           Console.WriteLine("sayı1 sayı2'ye tam bölünür");
+            }
+            else
+            {
+                Console.WriteLine("sayı1 sayı2'ye tam bölünmez");
+            }
+
+           
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
